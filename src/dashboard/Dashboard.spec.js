89b152c08d3e1044540408f30c44373e3b85e0d6
Jonathan Dorety <kyullog@gmail.com>
2019-04-16 22:39:19 -0400

    Complete Dashboard test suite

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,2 +1,25 @@
 // Test away
-import React from 'react';
+import React from "react";
+import renderer from "react-test-renderer";
+import { render, cleanup } from 'react-testing-library';
+import Dashboard from "./Dashboard";
+
+describe("The Dashboard component", () => {
+  it("matches the snapshot", () => {
+    const dash = renderer.create(<Dashboard />);
+    expect(dash.toJSON()).toMatchSnapshot();
+  });
+
+  it("displays Controls component", () => {
+    const { getByText } = render(<Dashboard />);
+    getByText(/Lock Gate/i);
+    getByText(/Close Gate/i);
+    cleanup();
+  });
+
+  it('displays Display component', () => {
+    const { getByText } = render(<Dashboard />);
+    getByText('Unlocked');
+    getByText(/Open/i);
+  })
+});
