2fe80c2b492fe3c0c03c01a9c5075e620d01ad63
Jonathan Dorety <kyullog@gmail.com>
2019-04-16 23:40:38 -0400

    Buttons in Controls component tested

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,28 @@
 // Test away!
+import React from "react";
+import { render, fireEvent, cleanup } from "react-testing-library";
+
+import Controls from "./Controls";
+
+describe("The Controls Component", () => {
+  it("provides button to toggle locked state", () => {
+    const mockLock = jest.fn();
+    const { getByText } = render(
+      <Controls
+        closed
+        toggleLocked={mockLock}
+      />
+    );
+    const lockButton = getByText(/lock gate/i);
+    fireEvent.click(lockButton);
+    expect(mockLock).toHaveBeenCalled();
+    cleanup();
+  });
+  it('provides button to toggle closed state', () => {
+    const mockClose = jest.fn();
+    const { getByText } = render(<Controls toggleClosed={mockClose} />)
+    const closeButton = getByText(/close gate/i);
+    fireEvent.click(closeButton);
+    expect(mockClose).toHaveBeenCalled();
+  })
+});
